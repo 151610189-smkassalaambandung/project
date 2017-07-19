b4b717fe99ad38605595c3c5aabfd1405c2a2afc
151610178-smkassalaambandung <151610178@smkassalaambandung.sch.id>
2017-07-20 03:47:52 +0700

    project pusing :(project pusing :(project pusing :(project pusing :(project pusing :(project pusing :(project pusing :(project pusing :(project pusing :(project pusing :(project pusing :(

---
+++ resources/views/paket.blade.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Wedding Organizer</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="a/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="a/css/business-casual.css" rel="stylesheet">
+
+    <!-- Fonts -->
+    <link href="a/https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css">
+    <link href="a/https://fonts.googleapis.com/css?family=Josefin+Slab:100,300,400,600,700,100italic,300italic,400italic,600italic,700italic" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <div class="brand">PAKET</div>
+    <div class="address-bar"></div>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-default" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <!-- navbar-brand is hidden on larger screens, but visible when the menu is collapsed -->
+                <a class="navbar-brand" href="index.html">Wedding Organizer</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="paket">Paket</a>
+                    </li>
+                    <li>
+                        <a href="about.html">Galeri</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <div class="container">
+
+        <div class="row">
+            <div class="box">
+                <div class="col-lg-12 text-center">
+                    <div id="carousel-example-generic" class="carousel slide">
+                        <!-- Indicators -->
+                        <ol class="carousel-indicators hidden-xs">
+                            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                        </ol>
+
+                        <!-- Wrapper for slides -->
+    <div class="row">
+        <div class="box">
+            <div class="col-lg-12">
+                <hr>
+                    <h2 class="intro-text text-center">PAKET GOLD HARGA MULAI</h2>
+                <hr>
+                    <img class="img-responsive img-border img-left" src="a/img/1.jpg" alt="">
+                <hr class="visible-xs">
+                <center><h5>GEDUNG DAN SARANA PESTA
+                BUFFER UTAMA 450 PORSI CATERING TIPE A
+                JASA PELAYANAN BOGA
+                DEKORASI:
+                    a. Dekorasi Buffe Utama 2 Set
+                    b. Dekorasi Buffe Pengantin
+                    c. Dekorasi Dessert
+                    d. Bunga segar 2 buah untuk meja tamu
+                    e. Pergola jalan canopy/gazebo
+                    f. Ice carving + Ice tray
+                    g. Janur umbul-umbul 1 pasang
+                    h. Janur Pintu 1 Pasang & rombe pintu</h5></center>
+            </div>
+        </div>
+    </div>
+
+                        <!-- Controls -->
+
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="box">
+                <div class="col-lg-12">
+                    <hr>
+                    <h2 class="intro-text text-center">Apa itu Wedding Organizer?
+                    </h2>
+                    <hr>
+                    <img class="img-responsive img-border img-left" src="a/img/1.jpg" alt="">
+                    <hr class="visible-xs">
+                    <p>Wedding Organizer adalah suatu jasa khusus yang secara pribadi membantu calon pengantin dan keluarga dalam perencanaan dan supervisi pelaksanaan rangkaian acara pesta pernikahan sesuai dengan jadwal yang telah ditetapkan. ... Membantu menyusun buku acara Dll yang berhubungan dengan acara pernikahan.</p>
+                    <p>Wedding Organizer Memberikan informasi mengenai berbagai macam hal yang berhubungan dengan acara pernikahan, Membantu merumuskan konsep pernikahan. Wedding Organizer mem-fasilitasi, negosiasi dan koordinasi dengan pihak gedung/hotel dan supplier/vendor seperti : Catering, dekorasi, fotografer, perias, grup musik, dll.</p>
+                    <p>Wedding Organizer Membantu memilih jenis makanan, kuantitas dan food tasting. Menghubungi setiap rekanan untuk follow up yang diperlukan, Mengikuti rapat koordinasi dengan keluarga, juga memimpin rapat teknis dengan para rekanan. Technical meeting di tempat resepsi. Membantu menyusun buku acara Dll yang berhubungan dengan acara pernikahan.
+
+                    Bagi Anda yg akan menikah dan belum menentukan apakah memakai sistem paket atau memilih sendiri vendor dan Wedding Organizer pernikahan, tips berikut ini kelebihan menggunakan Paket pernikahan dari sebuah Wedding Organizer?
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /.container -->
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <p>CONTACT PERSON : 089-514-999-152 LUXIOUS WO &copy;</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Script to Activate the Carousel -->
+    <script>
+    $('.carousel').carousel({
+        interval: 5000 //changes the speed
+    })
+    </script>
+
+</body>
+
+</html>
